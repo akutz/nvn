c2906535cb956ae493e3786d41b1342ffa2e4315
Schley Andrew Kutz <sakutz@gmail.com>
2010-02-10 17:47:31 +0000

    - mstest now searches for modular test projects and invokes their test containers directly instead of sending a test goal to the module. this avoids double compilation

--- nvn-plugins/nvn-mstest-plugin/src/main/java/net/sf/nvn/plugins/mstest/MSTestMojo.java
+++ nvn-plugins/nvn-mstest-plugin/src/main/java/net/sf/nvn/plugins/mstest/MSTestMojo.java
@@ -1,11 +1,13 @@
 package net.sf.nvn.plugins.mstest;
 
+import static net.sf.nvn.commons.StringUtils.quote;
 import java.io.File;
-import java.util.Collection;
-import org.apache.commons.io.FileUtils;
+import java.util.ArrayList;
+import java.util.List;
+import net.sf.nvn.commons.dotnet.v35.msbuild.MSBuildProject;
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
-import static net.sf.nvn.commons.StringUtils.quote;
+import org.apache.maven.project.MavenProject;
 
 /**
  * A Maven plug-in for testing .NET solutions and/or projects with MSTest.
@@ -14,34 +16,35 @@ import static net.sf.nvn.commons.StringUtils.quote;
  * @phase test
  * @description A Maven plug-in for testing .NET solutions and/or projects with
  *              MSTest.
+ * @requiresDependencyResolution
  */
 public class MSTestMojo extends AbstractExeMojo
 {
     /**
-     * An assembly that contains tests.
+     * One or more assemblies that contains tests.
      * 
-     * @parameter expression="${mstest.testContainer}"
+     * @parameter
      */
-    File testContainer;
+    File[] testContainers;
 
     /**
      * One or more files with the extension "vsmdi" that contains test metadata.
      * 
-     * @parameter expression="${mstest.testMetaDatas}"
+     * @parameter
      */
     File[] testMetaDatas;
 
     /**
      * A list of tests, as specified in the test metadata file, to be run.
      * 
-     * @parameter expression="${mstest.testLists}"
+     * @parameter
      */
     String[] testLists;
 
     /**
      * A list of individual tests to run.
      * 
-     * @parameter expression="${mstest.tests}"
+     * @parameter
      */
     String[] tests;
 
@@ -56,21 +59,21 @@ public class MSTestMojo extends AbstractExeMojo
      * Run tests within the MSTest.exe process. This choice improves test run
      * speed but increases risk to the MSTest.exe process.
      * 
-     * @parameter expression="${mstest.noIsolation}" default-value="false"
+     * @parameter default-value="false"
      */
     boolean noIsolation;
 
     /**
      * A run configuration file.
      * 
-     * @parameter expression="${mstest.runConfig}"
+     * @parameter
      */
     File runConfig;
 
     /**
      * Save the test run results to this file.
      * 
-     * @parameter expression="${mstest.resultsFile}"
+     * @parameter
      */
     File resultsFile;
 
@@ -78,28 +81,28 @@ public class MSTestMojo extends AbstractExeMojo
      * Instructs MSTest to run only a single test whose name matches one of the
      * tests you supply in the tests parameter.
      * 
-     * @parameter expression="${mstest.unique}"
+     * @parameter
      */
     boolean unique;
 
     /**
      * Display additional test case properties, if they exist.
      * 
-     * @parameter expression="${mstest.details}"
+     * @parameter
      */
     String[] details;
 
     /**
      * Display no startup banner and copyright message.
      * 
-     * @parameter expression="${mstest.noLogo}" default-value="false"
+     * @parameter default-value="false"
      */
     boolean noLogo;
 
     /**
      * The name of a Team Foundation Server to publish the test results to.
      * 
-     * @parameter expression="${mstest.publish}"
+     * @parameter
      */
     String publish;
 
@@ -107,14 +110,14 @@ public class MSTestMojo extends AbstractExeMojo
      * Specify the results file name to be published. If no results file name is
      * specified, use the file produced by the current run.
      * 
-     * @parameter expression="${mstest.publishResultsFile}"
+     * @parameter
      */
     File publishResultsFile;
 
     /**
      * Publish test results using this build ID.
      * 
-     * @parameter expression="${mstest.publishBuild}"
+     * @parameter
      */
     String publishBuild;
 
@@ -122,7 +125,7 @@ public class MSTestMojo extends AbstractExeMojo
      * Specify the flavor of the build against which test results should be
      * published.
      * 
-     * @parameter expression="${mstest.flavor}"
+     * @parameter
      */
     String flavor;
 
@@ -130,21 +133,91 @@ public class MSTestMojo extends AbstractExeMojo
      * Specify the platform of the build against which test results should be
      * published.
      * 
-     * @parameter expression="${mstest.platform}"
+     * @parameter
      */
     String platform;
 
     /**
      * Specify the name of the team project to which the build belongs.
      * 
-     * @parameter expression="${mstest.teamProject}"
+     * @parameter
      */
     String teamProject;
 
     @Override
     void preExecute() throws MojoExecutionException
     {
-        loadTestMetaData();
+        if (!forwardToTestProjects())
+        {
+            initTestContainer();
+        }
+    }
+
+    boolean forwardToTestProjects() throws MojoExecutionException
+    {
+        if (super.isHierarchicalSolution())
+        {
+            return false;
+        }
+
+        if (this.testMetaDatas != null && this.testMetaDatas.length > 0)
+        {
+            return false;
+        }
+
+        List<File> tcList = new ArrayList<File>();
+
+        for (MavenProject mp : getModules())
+        {
+            if (!mp.getPackaging().equalsIgnoreCase("mstest"))
+            {
+                continue;
+            }
+
+            if (getMSBuildModules().containsKey(mp.getArtifactId()))
+            {
+                MSBuildProject msbp =
+                    getMSBuildModules().get(mp.getArtifactId());
+                File tc =
+                    msbp.getBuildArtifact(getActiveBuildConfigurationName());
+                tc =
+                    new File(msbp.getProjectFile().getParentFile(), tc
+                        .getPath());
+                tcList.add(tc);
+            }
+        }
+
+        if (tcList.size() > 0)
+        {
+            this.testContainers = tcList.toArray(new File[0]);
+        }
+
+        return true;
+    }
+
+    void initTestContainer()
+    {
+        if (this.testContainers != null)
+        {
+            return;
+        }
+
+        if (!this.mavenProject.getPackaging().equals("mstest"))
+        {
+            return;
+        }
+
+        if (this.testMetaDatas != null && this.testMetaDatas.length > 0)
+        {
+            return;
+        }
+
+        this.testContainers =
+            new File[]
+            {
+                super.msbuildProject
+                    .getBuildArtifact(getActiveBuildConfigurationName())
+            };
     }
 
     @Override
@@ -161,7 +234,7 @@ public class MSTestMojo extends AbstractExeMojo
             return true;
         }
 
-        if (this.testContainer != null)
+        if (this.testContainers != null && this.testContainers.length > 0)
         {
             return true;
         }
@@ -175,11 +248,14 @@ public class MSTestMojo extends AbstractExeMojo
     {
         StringBuilder cmdLineBuff = new StringBuilder();
 
-        if (this.testContainer != null)
+        if (this.testContainers != null && this.testContainers.length > 0)
         {
-            cmdLineBuff.append("/testcontainer:");
-            cmdLineBuff.append(getPath(this.testContainer));
-            cmdLineBuff.append(" ");
+            for (File f : this.testContainers)
+            {
+                cmdLineBuff.append("/testcontainer:");
+                cmdLineBuff.append(getPath(f));
+                cmdLineBuff.append(" ");
+            }
         }
 
         if (this.testMetaDatas != null && this.testMetaDatas.length > 0)
@@ -305,50 +381,6 @@ public class MSTestMojo extends AbstractExeMojo
     String getMojoName()
     {
         return "mstest";
-    }
-
-    /**
-     * Loads the test metadata file.
-     */
-    void loadTestMetaData()
-    {
-        if (this.testMetaDatas != null && this.testMetaDatas.length > 0)
-        {
-            return;
-        }
-
-        this.testMetaDatas = findVsmdi();
-    }
-
-    /**
-     * Finds one or more test list files.
-     * 
-     * @return One or more test list files.
-     */
-    @SuppressWarnings("unchecked")
-    File[] findVsmdi()
-    {
-        Collection vsmdiFiles =
-            FileUtils.listFiles(super.mavenProject.getBasedir(), new String[]
-            {
-                "vsmdi"
-            }, false);
-
-        if (vsmdiFiles == null || vsmdiFiles.size() == 0)
-        {
-            return null;
-        }
-
-        File[] files = new File[vsmdiFiles.size()];
-
-        int x = 0;
-
-        for (Object f : vsmdiFiles)
-        {
-            files[x] = (File) f;
-        }
-
-        return files;
     }
 
     @Override
